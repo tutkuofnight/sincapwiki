1d0ebd24fd8b4cd58544e2bacd71d5796b3bcf8a
Tutku Ucan <mettheus0@gmail.com>
2024-12-29 21:49:53 +0300

    update ui and add metadata

--- app/routes/sincap.$slug.tsx
+++ app/routes/sincap.$slug.tsx
@@ -4,14 +4,19 @@ import { Sincap } from "~/types"
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import { ArrowUpRight } from "lucide-react"
+import type { MetaFunction } from "@remix-run/node"
 
 export const loader = ({ params }: LoaderFunctionArgs) => {
     const slug = params["slug"]
-    return  cfClient.getEntries<Sincap>({
+    return cfClient.getEntries<Sincap>({
         content_type: "sincaps",
         "fields.slug[match]": slug,
         limit: 1
     })
+}
+
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+    return [{ title: data?.items[0].fields.name + " | SincapWiki" }]
 }
 
 export default function(){
@@ -19,7 +24,7 @@ export default function(){
     const sincap = data.items[0]
 
     return (
-        <div className="w-full flex flex-col items-center justify-center gap-10">
+        <div className="w-full flex flex-col items-center  justify-center lg:flex-row lg:items-start gap-10">
             <section className="w-full md:w-[500px] flex justify-center">
                     <img src={sincap.fields.image.fields.file.url} alt={sincap.fields.name} className="w-auto max-h-[400px]" />
             </section>
@@ -30,10 +35,10 @@ export default function(){
                 <div className="sincap-content">
                     {documentToReactComponents(sincap.fields.about)}
                 </div>
+                <a href={sincap.fields.referenceLink} target="_blank" rel="noopener noreferrer" className="inline-flex items-center gap-1 bg-text rounded-md text-white p-1 px-3">
+                    Reference Link <ArrowUpRight className="w-4 h-4" />
+                </a>
             </aside>
-            <a href={sincap.fields.referenceLink} target="_blank" rel="noopener noreferrer" className="flex items-center gap-1">
-                Reference Link <ArrowUpRight className="w-4 h-4" />
-            </a>
         </div>
     )
 }
